e0cb0b3d485474b8030dc875da570d326cda66d0
Christopher Santana <christopher.santana@nav.no>
2022-02-24 09:57:13 +0100

    Fiks generic type error.

--- src/main/kotlin/no/nav/tms/eventtestproducer/common/HandlerConsumer.kt
+++ src/main/kotlin/no/nav/tms/eventtestproducer/common/HandlerConsumer.kt
@@ -2,6 +2,10 @@ package no.nav.tms.eventtestproducer.common
 
 import io.ktor.client.*
 import no.nav.brukernotifikasjon.schemas.builders.domain.Eventtype
+import no.nav.brukernotifikasjon.schemas.builders.domain.Eventtype.*
+import no.nav.tms.eventtestproducer.beskjed.Beskjed
+import no.nav.tms.eventtestproducer.innboks.Innboks
+import no.nav.tms.eventtestproducer.oppgave.Oppgave
 import no.nav.tms.eventtestproducer.tokenx.AccessToken
 import java.net.URL
 
@@ -9,8 +13,18 @@ class HandlerConsumer(
     private val client: HttpClient,
     private val eventhandlerBaseURL: URL) {
 
-    suspend fun <T> getActiveEvents(eventtype: Eventtype, accessToken:AccessToken): List<T> {
-        val pathToEndpoint = URL("$eventhandlerBaseURL/fetch/${eventtype.name.lowercase()}/aktive")
+    suspend fun getActiveBeskjedEvents(accessToken:AccessToken): List<Beskjed> {
+        val pathToEndpoint = URL("$eventhandlerBaseURL/fetch/beskjed/aktive")
+        return client.get(pathToEndpoint, accessToken)
+    }
+
+    suspend fun getActiveOppgaveEvents(accessToken:AccessToken): List<Oppgave> {
+        val pathToEndpoint = URL("$eventhandlerBaseURL/fetch/oppgave/aktive")
+        return client.get(pathToEndpoint, accessToken)
+    }
+
+    suspend fun getActiveInnboksEvents(accessToken:AccessToken): List<Innboks> {
+        val pathToEndpoint = URL("$eventhandlerBaseURL/fetch/innboks/aktive")
         return client.get(pathToEndpoint, accessToken)
     }
 }
--- src/main/kotlin/no/nav/tms/eventtestproducer/done/DoneEventService.kt
+++ src/main/kotlin/no/nav/tms/eventtestproducer/done/DoneEventService.kt
@@ -1,11 +1,7 @@
 package no.nav.tms.eventtestproducer.done
 
-import no.nav.brukernotifikasjon.schemas.builders.domain.Eventtype
-import no.nav.tms.eventtestproducer.beskjed.Beskjed
 import no.nav.tms.eventtestproducer.common.HandlerConsumer
 import no.nav.tms.eventtestproducer.common.InnloggetBruker
-import no.nav.tms.eventtestproducer.innboks.Innboks
-import no.nav.tms.eventtestproducer.oppgave.Oppgave
 import no.nav.tms.eventtestproducer.tokenx.EventhandlerTokendings
 
 class DoneEventService(
@@ -16,9 +12,9 @@ class DoneEventService(
 
     suspend fun markAllBrukernotifikasjonerAsDone(innloggetBruker: InnloggetBruker) {
         val exchangedToken = eventhandlerTokendings.exchangeToken(innloggetBruker)
-        val activeBeskjedEvents = handlerConsumer.getActiveEvents<Beskjed>(Eventtype.BESKJED, exchangedToken)
-        val activeOppgaveEvents = handlerConsumer.getActiveEvents<Oppgave>(Eventtype.OPPGAVE, exchangedToken)
-        val activeInnboksEvents = handlerConsumer.getActiveEvents<Innboks>(Eventtype.INNBOKS, exchangedToken)
+        val activeBeskjedEvents = handlerConsumer.getActiveBeskjedEvents(exchangedToken)
+        val activeOppgaveEvents = handlerConsumer.getActiveOppgaveEvents(exchangedToken)
+        val activeInnboksEvents = handlerConsumer.getActiveInnboksEvents(exchangedToken)
         val alleBrukernotifikasjoner = activeBeskjedEvents + activeOppgaveEvents + activeInnboksEvents
 
         alleBrukernotifikasjoner.forEach { brukernotifikasjon ->
